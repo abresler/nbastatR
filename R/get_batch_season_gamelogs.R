493f71fc4dc9a992bbc5e1234a290d92a3ab0ba9
Alex Bresler <abresler@asbcllc.com>
2015-10-28 19:50:27 -0400

    fix

--- R/get_batch_season_gamelogs.R
+++ R/get_batch_season_gamelogs.R
@@ -9,6 +9,270 @@
 #' @export
 #'
 #' @examples
+get_headers <- function() {
+  headers_df <-
+    data_frame(
+      name.nba = c(
+        "PLAYER_ID",
+        "SEASON_ID",
+        "LEAGUE_ID",
+        "TEAM_ID",
+        "TEAM_ABBREVIATION",
+        "PLAYER_AGE",
+        "GP",
+        "GS",
+        "MIN",
+        "FGM",
+        "FGA",
+        "FG_PCT",
+        "FG3M",
+        "FG3A",
+        "FG3_PCT",
+        "FTM",
+        "FTA",
+        "FT_PCT",
+        "OREB",
+        "DREB",
+        "REB",
+        "AST",
+        "STL",
+        "BLK",
+        "TOV",
+        "PF",
+        "PTS",
+        "ORGANIZATION_ID",
+        "SCHOOL_NAME",
+        "RANK_MIN",
+        "RANK_FGM",
+        "RANK_FGA",
+        "RANK_FG_PCT",
+        "RANK_FG3M",
+        "RANK_FG3A",
+        "RANK_FG3_PCT",
+        "RANK_FTM",
+        "RANK_FTA",
+        "RANK_FT_PCT",
+        "RANK_OREB",
+        "RANK_DREB",
+        "RANK_REB",
+        "RANK_AST",
+        "RANK_STL",
+        "RANK_BLK",
+        "RANK_TOV",
+        "RANK_PTS",
+        "RANK_EFF",
+        "PLUS_MINUS",
+        "WL",
+        "MATCHUP",
+        "VIDEO_AVAILABLE",
+        "GAME_DATE",
+        "Game_ID",
+        "PERSON_ID",
+        "FIRST_NAME",
+        "LAST_NAME",
+        "DISPLAY_FIRST_LAST",
+        "DISPLAY_LAST_COMMA_FIRST",
+        "DISPLAY_FI_LAST",
+        "BIRTHDATE",
+        "SCHOOL",
+        "COUNTRY",
+        "LAST_AFFILIATION",
+        "HEIGHT",
+        "WEIGHT",
+        "SEASON_EXP",
+        "JERSEY",
+        "POSITION",
+        "ROSTERSTATUS",
+        "TEAM_NAME",
+        "TEAM_CODE",
+        "TEAM_CITY",
+        "PLAYERCODE",
+        "FROM_YEAR",
+        "TO_YEAR",
+        "DLEAGUE_FLAG",
+        "GAMES_PLAYED_FLAG",
+        "PLAYER_NAME",
+        "TimeFrame",
+        "PIE"
+      ),
+      name.actual = c(
+        "id.player",
+        "code.season",
+        "id.league",
+        "id.team",
+        "slug.team",
+        "age.player",
+        "gp",
+        "gs",
+        "min",
+        "fgm",
+        "fga",
+        "pct.fg",
+        "fg3m",
+        "fg3a",
+        "pct.fg3",
+        "ftm",
+        "fta",
+        "pct.ft",
+        "oreb",
+        "dreb",
+        "reb",
+        "ast",
+        "stl",
+        "blk",
+        "tov",
+        "pf",
+        "pts",
+        "id.organization",
+        "name.school",
+        "rank.min",
+        "rank.fgm",
+        "rank.fga",
+        "rank.pct.fg",
+        "rank.fg3m",
+        "rank.fg3a",
+        "rank.pct.fg3",
+        "rank.ftm",
+        "rank.fta",
+        "rank.pct.ft",
+        "rank.oreb",
+        "rank.dreb",
+        "rank_reb",
+        "rank.ast",
+        "rank.stl",
+        "rank.blk",
+        "rank.tov",
+        "rank.pts",
+        "rank.eff",
+        "plus.minus",
+        "wl",
+        "matchup",
+        "is.video_available",
+        "date.game",
+        "id.game",
+        "id.player",
+        "name.first",
+        "name.last",
+        "name.player",
+        "name.last.display",
+        "name.middle.display",
+        "date.birth",
+        "school",
+        "country",
+        "college.non_nba_team",
+        "height",
+        "weight.lbs",
+        "years.experience",
+        "jersey",
+        "position",
+        "status.roster",
+        "name.team",
+        "code.team",
+        "city.team",
+        "slug.player",
+        "year.from",
+        "year.to",
+        "has.d_league_data",
+        "gp.flag",
+        "name.player",
+        "id.season",
+        "pie"
+      ),
+      id.row = 1:length(name.actual)
+    )
+  return(headers_df)
+}
+
+get_nba_players_ids <- function(active_only = F) {
+  players.url <-
+    "http://stats.nba.com/stats/commonallplayers?IsOnlyCurrentSeason=0&LeagueID=00&Season=2015-16"
+
+  json_data <-
+    players.url %>%
+    jsonlite::fromJSON(simplifyDataFrame = T)
+
+  data <-
+    json_data$resultSets$rowSet %>%
+    data.frame %>%
+    tbl_df
+
+  headers <-
+    json_data$resultSets$headers %>%
+    unlist %>%
+    str_to_lower()
+
+  headers_df <-
+    get_headers()
+
+  actual_names <-
+    1:length(headers) %>%
+    purrr::map(
+      function(x)
+        data_frame(
+          name.actual =
+            headers_df %>%
+            mutate(name.nba = name.nba %>% str_to_lower) %>%
+            dplyr::filter(name.nba == headers[x]) %>%
+            .$name.actual
+        )
+    ) %>%
+    bind_rows()
+
+  names(data) <-
+    actual_names$name.actual
+
+  names_df <-
+    data$name.last.display %>%
+    str_split_fixed(pattern = '\\,', 2) %>%
+    data.frame() %>%
+    tbl_df
+
+  names(names_df) <-
+    c('name.last', 'name.first')
+
+  names_df %<>%
+    mutate(player = name.first %>% str_trim %>% paste(name.last %>% str_trim)) %>%
+    dplyr::select(player)
+  data$name.player <-
+    names_df$player
+  data %<>%
+    mutate(
+      id.player = id.player %>% as.numeric,
+      is.active_player = ifelse(id.team == 0, FALSE, TRUE),
+      id.team = id.team %>% as.numeric
+    ) %>%
+    dplyr::select(-c(status.roster, name.last.display)) %>%
+    mutate_each(funs(extract_numeric), starts_with("year.")) %>%
+    mutate(
+      id.team = ifelse(id.team == 0, NA, id.team),
+      city.team = ifelse(city.team == '', NA, city.team),
+      name.team = ifelse(name.team == '', NA, name.team),
+      code.team = ifelse(code.team == '', NA, code.team),
+      slug.team = ifelse(slug.team == '', NA, slug.team),
+      team = ifelse(city.team %>% is.na, NA, paste(city.team, name.team)),
+      seasons.played = year.to - year.from,
+      url.player = id.player %>% paste0('http://stats.nba.com/player/#!/', .)
+    ) %>%
+    dplyr::select(
+      name.player,
+      id.player,
+      team,
+      id.team,
+      is.active_player,
+      seasons.played,
+      year.from,
+      year.to,
+      everything()
+    )
+
+  if (active_only == T) {
+    data %<>%
+      dplyr::filter(is.active_player == T)
+  }
+
+  return(data)
+}
+
 get_batch_player_gamelogs <-
   function(year.season_start,
            season_type,
