f50d3d2fc2f4f5b4875af636c7a51be3b3e03284
Alex Bresler <abresler@asbcllc.com>
2015-11-03 15:54:21 -0500

    ok

--- R/get_nba_player_stat_tables.R
+++ R/get_nba_player_stat_tables.R
@@ -177,17 +177,6 @@ clean_to_stem <- function(x) {
 }
 get_nba_franchise_data <- function(return_franchises = c('all', 'active', 'current'),
                                    return_message = T){
-  packages <- #need all of these installed including some from github
-    c(
-      'dplyr',
-      'magrittr',
-      'jsonlite',
-      'tidyr',
-      'stringr',
-      'tidyr'
-    )
-  options(warn = -1)
-  lapply(packages, library, character.only = T)
   team_history_url <-
     'http://stats.nba.com/stats/franchisehistory?LeagueID=00'
 
