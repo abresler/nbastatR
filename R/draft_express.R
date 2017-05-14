968e061142129403943eaf799368d57a99a156e8
Alex Bresler <abresler@asbcllc.com>
2017-05-14 10:53:29 -0400

    Added measurements

--- R/draft_express.R
+++ R/draft_express.R
@@ -23,24 +23,17 @@ parse_to_inches <-
     portion_feet + inches + plus_inches
   }
 
-get_draft_urls <- function(year = NULL) {
-
-  if (year %>% purrr::is_null()) {
-    url <-
-      "http://www.draftexpress.com/nba-pre-draft-measurements/all/all/all"
-  } else {
-    url <-
-      glue::glue("http://www.draftexpress.com/nba-pre-draft-measurements/{year}/all/all/all")
-  }
-
-  max_numbers <-
+get_draft_urls <- function(url) {
+  page <-
     url %>%
     as.character() %>%
-    read_html() %>%
+    read_html()
+
+  max_numbers <-
+    page %>%
     html_nodes('.pagination a') %>%
     html_text() %>%
     str_trim()
-
 
   if (max_numbers %>% length() == 0) {
     max_numbers <- 1
@@ -63,6 +56,137 @@ get_draft_urls <- function(year = NULL) {
 
   urls
 }
+#' Draft Express Combine Sources
+#'
+#' @return
+#' @export
+#' @import dplyr
+#' @examples
+get_data_draft_express_combine_sources <-
+  function() {
+    df_sources <-
+      data_frame(
+        nameSource = c(
+          "All",
+          "Vince Carter Camp",
+          "USA Pan Am Team",
+          "USA Basketball",
+          "UA All-American Camp",
+          "Reebok Breakout",
+          "Portsmouth",
+          "Paul Pierce Camp",
+          "PG Skills Acad",
+          "Official Team",
+          "Official College Team",
+          "Nike Skills Academy",
+          "Nike Elite 100",
+          "Nike Basketball Academy",
+          "Newspaper",
+          "Nets Workout",
+          "NBA Top 100 Camp",
+          "NBA Pre-Draft Camp",
+          "NBA Draft Combine",
+          "LeBron James Camp",
+          "Kevin Durant Camp",
+          "Hoop Summit",
+          "Eurocamp",
+          "Elite 24",
+          "Deron Williams Camp",
+          "D-League Elite Camp",
+          "Clippers Workout",
+          "Biosteel All-Canadian Game",
+          "Big Man Skills Acad",
+          "Amare Stoudemire Camp"
+        ),
+        slugSource = c(
+          "all",
+          "Vince+Carter+Camp",
+          "USA+Pan+Am+Team",
+          "USA+Basketball",
+          "UA+All-American+Camp",
+          "Reebok+Breakout",
+          "Portsmouth",
+          "Paul+Pierce+Camp",
+          "PG+Skills+Acad",
+          "Official+Team",
+          "Official+College+Team",
+          "Nike+Skills+Academy",
+          "Nike+Elite+100",
+          "Nike+Basketball+Academy",
+          "Newspaper",
+          "Nets+Workout",
+          "NBA+Top+100+Camp",
+          "NBA+Pre-Draft+Camp",
+          "NBA+Draft+Combine",
+          "LeBron+James+Camp",
+          "Kevin+Durant+Camp",
+          "Hoop+Summit",
+          "Eurocamp",
+          "Elite+24",
+          "Deron+Williams+Camp",
+          "D-League+Elite+Camp",
+          "Clippers+Workout",
+          "Biosteel+All-Canadian+Game",
+          "Big+Man+Skills+Acad",
+          "Amare+Stoudemire+Camp"
+        )
+      )
+    df_sources
+  }
+
+
+generate_base_de_urls <-
+  function(year = 2016,
+           source = "NBA Draft Combine",
+           position = NULL) {
+    if (year %>% purrr::is_null()) {
+      year_slug <- 'all'
+    } else {
+      year_slug <-
+        year
+    }
+
+    if (position %>% purrr::is_null()) {
+      position_slug <- 'all'
+    } else {
+      positions <- c("ALL","C", "PF", "SF", "SG", "PG")
+      position_options <- positions %>% str_c(collapse = '\n')
+      if (!position %>% str_to_upper() %in% positions) {
+        stop(glue::glue("Sorry sources can only be:\n{position_options}"))
+      }
+      position_slug <-
+        position %>% str_to_lower()
+    }
+
+    if (source %>% purrr::is_null()) {
+      source <- 'All'
+    }
+    df_sources <-
+      get_data_draft_express_combine_sources()
+    sources <- df_sources$nameSource %>% str_to_lower()
+    if (!source %>% str_to_lower() %in% sources) {
+      source_options <- df_sources$nameSource %>% str_c(collapse = '\n')
+      stop(glue::glue("Sorry sources can only be:\n{source_options}"))
+    }
+    source_search <-
+      source %>% str_to_lower()
+
+    slug_source <-
+      df_sources %>%
+      mutate(nameSource = nameSource %>% str_to_lower()) %>%
+      filter(nameSource %in% source_search) %>%
+      .$slugSource
+
+    url <-
+      glue::glue(
+        'http://www.draftexpress.com/nba-pre-draft-measurements/{year_slug}/{slug_source}/{position_slug}/all'
+      )
+    urls <-
+      url %>%
+      get_draft_urls()
+
+    urls
+  }
 
 parse_page_data <-
   function(url = "http://www.draftexpress.com/nba-pre-draft-measurements/all/all/all/all/33",
@@ -165,7 +289,7 @@ parse_page_data <-
       select(-idRow) %>%
       dplyr::select(namePlayer, yearDraft, everything()) %>%
       suppressMessages() %>%
-      mutate(urlPage = url)
+      mutate(urlSearch = url)
   }
 
 parse_draft_pages <-
@@ -212,9 +336,52 @@ parse_de_id <- function(x = "http://www.draftexpress.com/profile/Scottie-Pippen-
 }
 
 #' Draft Express Measurements
-#' @param year draft years - if \code{NULL} all draft years
+#' @param year draft years - if \code{NULL} or \code{ALL} all draft years
+#' @param sources sources to search \itemize{
+#' \item NULL - All Sources
+#' \item All - All sources
+#' \item Vince Carter Camp
+#' \item USA Pan Am Team
+#' \item USA Basketball
+#' \item UA All-American Camp
+#' \item Reebok Breakout
+#' \item Portsmouth
+#' \item Paul Pierce Camp
+#' \item PG Skills Acad
+#' \item Official Team
+#' \item Official College Team
+#' \item Nike Skills Academy
+#' \item Nike Elite 100
+#' \item Nike Basketball Academy
+#' \item Newspaper
+#' \item Nets Workout
+#' \item NBA Top 100 Camp
+#' \item NBA Pre-Draft Camp
+#' \item NBA Draft Combine
+#' \item LeBron James Camp
+#' \item Kevin Durant Camp
+#' \item Hoop Summit
+#' \item Eurocamp
+#' \item Elite 24
+#' \item Deron Williams Camp
+#' \item D-League Elite Camp
+#' \item Clippers Workout
+#' \item Biosteel All-Canadian Game
+#' \item Big Man Skills Acad
+#' \item Amare Stoudemire Camp
+#' }
+#' @param positions - positions to return \itemize{
+#' \item NULL - All positions
+#' \item All - All
+#' \item PG - Point Guards
+#' \item SG - Shooting Guards
+#' \item SF - Small Forwards
+#' \item PF - Power Forwards
+#' \item C - Centers
+#' }
 #' @param return_unique_players if \code{TRUE} returns players last data
 #' @param return_message
+#' @references \href{http://www.draftexpress.com/}{Draft Express}
 #'
 #' @return
 #' @export
@@ -222,33 +389,71 @@ parse_de_id <- function(x = "http://www.draftexpress.com/profile/Scottie-Pippen-
 #' @examples
 get_data_draft_express_measurements <-
   function(years = 1987:2017,
+           sources = NULL,
+           positions = "all",
            return_unique_players = FALSE,
            return_message = TRUE) {
-    get_draft_urls_safe <-
-      purrr::possibly(get_draft_urls, NA)
 
-    if (years %>% purrr::is_null()){
-      urls <-
-      get_draft_urls(year = NULL)
-    } else {
-      urls <- years %>%
-        map_df(function(x){
-          urls <- get_draft_urls_safe(year = x )
-          data_frame(yearData = x, urlData = urls)
-        }) %>%
-        .$urlData
+    if (sources %>% purrr::is_null()) {
+      sources <- 'all'
     }
+
+    if (years %>% purrr::is_null()) {
+      years <- 'all'
+    }
+
+    if (positions %>% purrr::is_null()) {
+      positions <- 'all'
+    }
+    generate_base_de_urls_safe <-
+      purrr::possibly(generate_base_de_urls, data_frame())
+
+    df_options <-
+      expand.grid(yearData = years,
+                sourceData = sources,
+                idPosition = positions,
+                stringsAsFactors = FALSE) %>%
+      tbl_df()
+
+    df_search_urls <-
+      1:nrow(df_options) %>%
+      map_df(function(x){
+        year_search <-
+          df_options$yearData[[x]]
+        source_search <-
+          df_options$sourceData[[x]]
+        position_search <-
+          df_options$idPosition[[x]]
+        search_url <- generate_base_de_urls_safe(year = year_search,
+                                            source = source_search,
+                                            position = position_search)
+      data_frame(urlSearch = search_url,
+                 positionSearch = position_search,
+                 yearSearch = year_search,
+                 sourceSearch = source_search)
+      })
 
     parse_pages_data_safe <-
       purrr::possibly(parse_draft_pages, data_frame())
+
+    urls <- df_search_urls$urlSearch
 
     all_data <-
       urls %>%
       map_df(function(x) {
         parse_pages_data_safe(url = x, return_message = T)
-      })
+      }) %>%
+      suppressWarnings()
 
-    max_year <- all_data$yearDraft %>% max(na.rm = T)
+    all_data <-
+      all_data %>%
+      tidyr::separate(namePlayer, into = c('namePlayer', 'detailPlayer'), sep = '\\(') %>%
+      mutate(detailPlayer = detailPlayer %>% str_replace_all('\\)','')) %>%
+      mutate_if(is.character,str_trim) %>%
+      suppressWarnings()
+
+    max_year <-
+      all_data$yearDraft %>% max(na.rm = T)
 
     is_predraft <-
       all_data %>%
@@ -291,13 +496,19 @@ get_data_draft_express_measurements <-
       all_data %>%
       mutate(
         namePlayer = namePlayer %>% str_replace_all('_ ', ' ') %>% str_trim(),
-        isFirstRoundPick = numberDraftPick <= 30,
-        isSecondRoundPick = numberDraftPick > 30,
-        isLotteryPick = numberDraftPick <= 14,
-        isTop5Pick =  numberDraftPick <= 5,
-        isNumber1Pick =  numberDraftPick == 1
+        isFirstRoundPick = ifelse(isDrafted, numberDraftPick <= 30, FALSE),
+        isSecondRoundPick = ifelse(isDrafted, numberDraftPick > 30, FALSE),
+        isLotteryPick = ifelse(isDrafted, numberDraftPick <= 14, FALSE),
+        isTop5Pick =  ifelse(isDrafted, numberDraftPick <= 5, FALSE),
+        isNumber1Pick =  ifelse(isDrafted, numberDraftPick == 1, FALSE)
       ) %>%
-      distinct()
+      distinct() %>%
+      dplyr::select(which(colMeans(is.na(.)) < 1))
+
+    all_data <-
+      all_data %>%
+      left_join(df_search_urls) %>%
+      suppressMessages()
 
 
     if (return_unique_players) {
