c790c17a05cf64091e1e8f23f8751019d07c4e58
Alex Bresler <abresler@asbcllc.com>
2020-03-17 22:30:10 -0400

    a

--- R/xis_ecmo.R
+++ R/xis_ecmo.R
@@ -1,9 +1,9 @@
 .curl_chinazi <-
-  function(url = "https://stats.nba.com/stats/leaguegamelog?Counter=1000&Season=2019-20&Direction=DESC&LeagueID=00&PlayerOrTeam=T&SeasonType=Regular%20Season&Sorter=DATE") {
+  function(url = "https://stats.nba.com/stats/leaguegamelog?Counter=1000&Season=2019-20&Direction=DESC&LeagueID=00&PlayerOrTeam=P&SeasonType=Regular%20Season&Sorter=DATE") {
 
 
     headers = c(
-      `Connection` = 'keep-alive',
+      `Connection` = 'close',
       `Accept` = 'application/json, text/plain, */*',
       `x-nba-stats-token` = 'true',
       `User-Agent` = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
@@ -13,6 +13,20 @@
       `Referer` = 'https://downwiththechinazis.com',
       `Accept-Encoding` = 'gzip, deflate, br',
       `Accept-Language` = 'en-US,en;q=0.9'
+    )
+
+    headers <- c(
+      `Host` = 'stats.nba.com',
+      `User-Agent` = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv =72.0) Gecko/20100101 Firefox/72.0',
+      `Accept` = 'application/json, text/plain, */*',
+      `Accept-Language` = 'en-US,en;q=0.5',
+      `Accept-Encoding` = 'gzip, deflate, br',
+      `x-nba-stats-origin` = 'stats',
+      `x-nba-stats-token` = 'true',
+      `Connection` = 'keep-alive',
+      `Referer` = 'https =//stats.nba.com/',
+      `Pragma` = 'no-cache',
+      `Cache-Control` = 'no-cache'
     )
 
     # headers = c(
