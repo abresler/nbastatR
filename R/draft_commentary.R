cb5ff54610fe74f343061b4660bafdd6cd626ec8
removedporn <86824510+removedporn@users.noreply.github.com>
2021-07-31 18:17:57 +0800

    Delete R directory

--- R/draft_commentary.R
+++
@@ -1,6 +0,0 @@
-
-.parse_draftpick_url <-
-  function(url = "https://data.nba.net/data/5s/prod/draft/2017/draft_pick.json") {
-    data <- url %>% fromJSON(simplifyDataFrame = T) %>% as_tibble()
-    data
-  }
